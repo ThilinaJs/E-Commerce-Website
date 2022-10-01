1372c85f88b818be87500f57ff559dac73c2fb32
ThilinaJs <thilina.jayasooriya256@gmail.com>
2022-10-01 15:33:04 +0530

    Created main templates
    Home and About HTML's created

--- ecommerce/ecomapp/urls.py
+++ ecommerce/ecomapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import *
 
 app_name = "ecomapp"
 urlpatterns = [
-    
+    path("", HomeView.as_view(),name="home"),
+    path("about/", AboutView.as_view(), name = "about")
+
 ]
