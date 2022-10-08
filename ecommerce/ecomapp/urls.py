815840635a3002abf6b2f19967f541455c3815aa
ThilinaJs <thilina.jayasooriya256@gmail.com>
2022-10-08 07:39:52 +0530

    Added all products to the home page
    Also adjust the homepage layer out

--- ecommerce/ecomapp/urls.py
+++ ecommerce/ecomapp/urls.py
@@ -5,6 +5,7 @@ app_name = "ecomapp"
 urlpatterns = [
     path("", HomeView.as_view(),name="home"),
     path("about/", AboutView.as_view(), name = "about"),
-    path("contact-us/", ContactView.as_view(), name="contact")
+    path("contact-us/", ContactView.as_view(), name="contact"),
+    path("all-products/", AllProductsView.as_view(), name=allproducts ),
 
 ]
--- ecommerce/ecomapp/views.py
+++ ecommerce/ecomapp/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from ecomapp.models import Category, Product
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list']=Product.objects.all()
+        context['allcategories'] = Category.objects.all()
+        return context
+
+class AllProductsView(TemplateView):
+    template_name = "allproducts.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs
+        )
+
+
 
 class AboutView(TemplateView):
     template_name = "about.html"
 
 class ContactView(TemplateView):
     template_name = "contactus.html"
+
