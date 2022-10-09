313998a5040125d9e1b23740b75f39d3d01f15af
ThilinaJs <>
2022-10-09 05:45:31 +0530

    Cart functionalities added

--- ecommerce/ecomapp/urls.py
+++ ecommerce/ecomapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("contact-us/", ContactView.as_view(), name="contact"),
     path("all-products/", AllProductsView.as_view(), name="allproducts" ),
     path("product/<slug:slug>/", ProductDetailView.as_view(), name ="productdetail"),
-    path("add-to-cart-<int:pro_id>/", AddToCartView.as_view(), name="addtocart")
+    path("add-to-cart-<int:pro_id>/", AddToCartView.as_view(), name="addtocart"),
+    path("my-cart/", MyCartView.as_view(),name = "mycart")
 
 ]
