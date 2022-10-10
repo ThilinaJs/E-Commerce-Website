5b478d6f2703131c6e7c76f14b677771d3dcc78b
ThilinaJs <>
2022-10-10 15:55:55 +0530

    Update models.py

--- ecommerce/ecomapp/models.py
+++ ecommerce/ecomapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length =200)
